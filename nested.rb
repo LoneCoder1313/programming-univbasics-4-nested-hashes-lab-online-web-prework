664cb5e449ffeaf982574dd4635d5fb39c5a84ab
Ricky dildy <rdildy92@gmail.com>
2019-09-26 01:20:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,8 +19,9 @@ def hopper
 
 end
 
-def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+def alan_kay_is_known_for=
+	{
+	  :languages =>["Smalltalk", "LISP"]
 	
 	programmer_hash = 
  		{
